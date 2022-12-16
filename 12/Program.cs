87b989c357b5f46c0c949ac0ad9ae39501e4d5ba
Robert_Hedgate <robert.h@nowaste.se>
2022-12-16 10:40:11 +0100

    12

---
+++ 12/Program.cs
@@ -0,0 +1,141 @@
+﻿using _12;
+using System.Diagnostics.CodeAnalysis;
+
+var result = int.MaxValue;
+
+var helpers = new Helpers.File();
+//var data = helpers.Read("Data.txt");
+var lines = helpers.ReadLines("Data.txt");
+
+var map = new char[lines.Count(), lines[0].Length];
+
+var lineNumber = 0;
+var columnNumber = 0;
+var start = (0, 0);
+var end = (0, 0);
+var aList = new List<(int, int)>();
+foreach (var line in lines)
+{
+    columnNumber = 0;
+    foreach (var c in line)
+    {
+        if (c == 'a')
+        {
+            if (columnNumber == 0)
+            {
+                aList.Add((lineNumber, columnNumber));
+            }
+        }
+        if (c == 'S')
+        {
+            start = (lineNumber, columnNumber);
+            aList.Add((lineNumber, columnNumber));
+        }
+        if (c == 'E')
+        {
+            end = (lineNumber, columnNumber);
+        }
+        map[lineNumber, columnNumber] = c;
+        columnNumber += 1;
+    }
+    lineNumber += 1;
+}
+
+var ways = new List<Way>();
+var oriWay = new Way();
+oriWay.SetMap(map);
+oriWay.Path.Add(start);
+ways.Add(oriWay);
+
+while (!ways.All(w => w.Stopped))
+{
+    var newWays = new List<Way>();
+    foreach (var way in ways)
+    {
+        var moreWays = way.Move();
+        newWays.AddRange(moreWays);
+    }
+
+    foreach (var newWay in newWays)
+    {
+        var found = false;
+        foreach (var testWay in ways)
+        {
+            if (testWay.Path.Contains(newWay.Path.Last()))
+            {
+                found = true;
+            }
+        }
+        if (!found)
+        {
+            ways.Add(newWay);
+        }
+    }
+}
+
+foreach (var way in ways)
+{
+    if (map[way.Path.Last().Item1, way.Path.Last().Item2] == 'E')
+    {
+        if (way.Path.Count() < result)
+        {
+            result = way.Path.Count() - 1;
+        }
+    }
+}
+Console.WriteLine($"Result part 1: {result}");
+
+result = int.MaxValue;
+
+var index = 0;
+foreach (var aPos in aList)
+{
+    index++;
+    ways = new List<Way>();
+    oriWay = new Way();
+    oriWay.SetMap(map);
+    oriWay.Path.Add(aPos);
+    oriWay.Part2 = true;
+    ways.Add(oriWay);
+
+    while (!ways.All(w => w.Stopped))
+    {
+        var newWays = new List<Way>();
+        foreach (var way in ways)
+        {
+            var moreWays = way.Move();
+            newWays.AddRange(moreWays);
+        }
+
+        foreach (var newWay in newWays)
+        {
+            var found = false;
+            foreach (var testWay in ways)
+            {
+                if (testWay.Path.Contains(newWay.Path.Last()))
+                {
+                    found = true;
+                }
+            }
+            if (!found)
+            {
+                ways.Add(newWay);
+            }
+        }
+    }
+
+    foreach (var way in ways)
+    {
+        if (map[way.Path.Last().Item1, way.Path.Last().Item2] == 'E')
+        {
+            if (way.Path.Count() < result)
+            {
+                result = way.Path.Count() - 1;
+            }
+        }
+    }
+}
+    
+Console.WriteLine($"Result part 2: {result}");
+
+
---
+++ 12/Way.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12
+{
+    internal class Way
+    {
+        public List<(int, int)> Path = new List<(int, int)>();
+        public static char[,] Map;
+        public bool Stopped = false;
+        public bool Part2 = false;
+
+        public Way()
+        {
+        }
+
+        public void SetMap(char[,] map)
+        {
+            Map = map;
+        }
+
+        internal List<Way> Move()
+        {
+            if (Stopped)
+            {
+                return new List<Way>();
+            }
+            var getDirections = GetPossibleDirections(Path.Last());
+
+            int i = 0;
+            var lastPos = Path.Last();
+            var newPath = new List<Way>();
+            foreach (var direction in getDirections)
+            {
+                if (Path.Contains(direction))
+                {
+                    continue;
+                }
+                if (i == 0)
+                {
+                    Path.Add(direction);
+                }
+                else
+                {
+                    var way = new Way();
+                    way.Path.AddRange(Path.Take(Path.Count() - 1));
+                    way.Path.Add(direction);
+                    way.Part2 = Part2;
+                    newPath.Add(way);
+                }
+                i++;
+            }
+
+            if (Part2 && Path.Count() > 445)
+            {
+                Stopped = true;
+            }
+            if (Part2 && Map[Path.Last().Item1, Path.Last().Item2] == 'a')
+            {
+                Stopped = true;
+            }
+            if (lastPos == Path.Last() || Map[Path.Last().Item1, Path.Last().Item2] == 'E')
+            {
+                Stopped= true;
+            }
+
+            return newPath;
+            //foreach (var path in newPath)
+            //{
+            //    path.Move();
+            //}
+        }
+
+        private List<(int, int)> GetPossibleDirections((int, int) start)
+        {
+            var ways = new List<(int, int)>();
+            if (start.Item1 - 1 >= 0)
+            {
+                if (CanGoHere(Map[start.Item1, start.Item2], Map[start.Item1 - 1, start.Item2]))
+                {
+                    ways.Add((start.Item1 - 1, start.Item2));
+                }
+            }
+            if (start.Item2 + 1 < Map.GetLength(1))
+            {
+                if (CanGoHere(Map[start.Item1, start.Item2], Map[start.Item1, start.Item2 + 1]))
+                {
+                    ways.Add((start.Item1, start.Item2 + 1));
+                }
+            }
+            if (start.Item1 + 1 < Map.GetLength(0))
+            {
+                if (CanGoHere(Map[start.Item1, start.Item2], Map[start.Item1 + 1, start.Item2]))
+                {
+                    ways.Add((start.Item1 + 1, start.Item2));
+                }
+            }
+            if (start.Item2 - 1 >= 0)
+            {
+                if (CanGoHere(Map[start.Item1, start.Item2], Map[start.Item1, start.Item2 - 1]))
+                {
+                    ways.Add((start.Item1, start.Item2 - 1));
+                }
+            }
+            return ways;
+        }
+
+        private bool CanGoHere(char from, char to)
+        {
+            if (from == 'S')
+            {
+                from = 'a';
+            }
+            if (to == 'E')
+            {
+                to = 'z';
+            }
+            //if (Part2)
+            //{
+            //    if (to == from || to - 1 == from)
+            //    {
+            //        return true;
+            //    }
+            //}
+            //else
+            {
+                if (to <= from || to - 1 == from)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
